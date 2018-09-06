36bf31be67fbf23ae1bc30720b470e6e71d9e72d
rikasun <ruijiasun1323@gmail.com>
2018-09-05 17:27:05 -0700

    Fix associations

--- associations/app/models/course.rb
+++ associations/app/models/course.rb
@@ -15,20 +15,19 @@ class Course < ApplicationRecord
     primary_key: :id,
     foreign_key: :course_id,
     class_name: :Enrollment
-    
+
   has_many :enrolled_students,
     through: :enrollments,
     source: :student
-    
+
   belongs_to :prerequisite,
     primary_key: :id,
     foreign_key: :prereq_id,
     class_name: :Course,
-    optional: true 
-    
+    optional: true
+
   belongs_to :instructor,
     primary_key: :id,
     foreign_key: :instructor_id,
     class_name: :User
 end
-d
